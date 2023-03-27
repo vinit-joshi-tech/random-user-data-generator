3e45e233a9e6786d722c6f504d4764573e706856
Vinit Joshi <vinit.joshi@5exceptions.com>
2023-03-27 12:38:33 +0530

    1.0.0 version updates done

---
+++ index.js
@@ -0,0 +1,25 @@
+const faker = require('faker');
+
+function generateUser() {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const address = faker.address.streetAddress();
+  const city = faker.address.city();
+  const state = faker.address.state();
+  const zipCode = faker.address.zipCode();
+
+  return {
+    firstName,
+    lastName,
+    email,
+    address,
+    city,
+    state,
+    zipCode
+  };
+}
+
+module.exports = {
+  generateUser
+};
